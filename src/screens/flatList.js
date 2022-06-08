50d9c38940a3159e392dfd8c2eb868a7f1bc1863
= <=>
2022-06-08 10:37:19 +0700

    update

--- src/screens/flatList.js
+++ src/screens/flatList.js
@@ -1,13 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, FlatList, Text, Image } from "react-native";
 
 export default function List() {
   // Make Dummy Data with Array
+  const data = [
+    {
+      name: "Ilham Fathullah"
+    },
+    {
+      name: "Yadi"
+    },
+    {
+      name: "Faldhi"
+    },
+  ]
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 }
