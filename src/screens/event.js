264293015c2ad9961f73327a620150781fb40a49
= <=>
2022-06-08 09:39:37 +0700

    update

--- src/screens/event.js
+++ src/screens/event.js
@@ -1,14 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function Event() {
   // Create Function Here
+  function Greeting() {
+    return alert("Good Morning")
+  }
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text>If you are press "Click Here" then the alert will be appear</Text>
+      <TouchableOpacity onPress={() => alert("Hello batch 34 FS")}>
+        <Text>Click Here</Text>
+      </TouchableOpacity>
+
+      <Text>If you are press "Click Me" then the alert will be appear</Text>
+      <TouchableOpacity onPress={Greeting}>
+        <Text>Click Me</Text>
+      </TouchableOpacity>
     </View>
   );
 }
