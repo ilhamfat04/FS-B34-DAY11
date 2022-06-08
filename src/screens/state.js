02e354a993c195e4854f07d2bf7d8a240339d1dc
= <=>
2022-06-08 10:04:51 +0700

    update

--- src/screens/state.js
+++ src/screens/state.js
@@ -1,18 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function State() {
   // Init State
+  const [counter, setCounter] = useState(0)
+  // let counter = 0
 
   //Create Function Add
+  function Add() {
+    return setCounter(counter + 1)
+  }
 
   //Create Function Less
+  function Less() {
+    return setCounter(counter - 1)
+  }
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text>
+        If you click add, it will increase. but if you click Less, it will be decrease
+      </Text>
+
+      <Text>{counter}</Text>
+
+      <TouchableOpacity onPress={Add}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={Less}>
+        <Text>Less</Text>
+      </TouchableOpacity>
     </View>
   );
 }
